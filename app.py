22c780dfc9391d64e4d31bc0cc4ae16636e0f322
nicolas-carabelli <108308637+nicolas-carabelli@users.noreply.github.com>
2024-02-12 13:55:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hola, RadioNet!'
+
+# Definir el handler de Lambda
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": jsonify(hello_world())
+    }
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
