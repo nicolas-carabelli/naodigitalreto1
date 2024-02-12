a98fb27f983771a46a52ed965d096dfc5567c4e7
nicolas-carabelli <108308637+nicolas-carabelli@users.noreply.github.com>
2024-02-12 14:29:12 -0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -10,8 +10,7 @@ class BasicTests(unittest.TestCase):
     def test_home(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data.decode())
-        self.assertEqual(data['message'], 'Hola, RadioNet!')
+        self.assertEqual(response.data.decode(), 'Hola, RadioNet!')
 
 if __name__ == '__main__':
     unittest.main()
