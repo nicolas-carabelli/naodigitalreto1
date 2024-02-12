22d1059ad093b6abbc5c717f5569b1a9aaaf59df
nicolas-carabelli <108308637+nicolas-carabelli@users.noreply.github.com>
2024-02-12 13:43:29 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,14 @@
 # Definir variables
 AWS_REGION="us-east-1"
 STACK_NAME="ci-cd"
+S3_BUCKET="bucketnaodigital" # Add your S3 bucket name here
 ECR_IMAGE_URI="714715362869.dkr.ecr.us-east-1.amazonaws.com/reponaodigital:$BUILD_ID"
 
 # Desplegar usando AWS CloudFormation o AWS SAM
-# Ejemplo con AWS SAM para una aplicación serverless
 sam deploy --template-file template.yaml \
            --stack-name $STACK_NAME \
            --region $AWS_REGION \
+           --s3-bucket $S3_BUCKET \  # Add this line
            --image-repository $ECR_IMAGE_URI \
            --no-fail-on-empty-changeset \
            --capabilities CAPABILITY_IAM
