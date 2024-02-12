cbed468e633e39b36ce7aee6bb576c9a775c9997
nicolas-carabelli <108308637+nicolas-carabelli@users.noreply.github.com>
2024-02-12 14:23:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ app = FlaskLambda(__name__)
 
 @app.route('/')
 def hello_world():
-    return jsonify(message='Hola, RadioNet!')
+    return 'Hola, RadioNet!'
 
 def lambda_handler(event, context):
     return app(event, context)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
